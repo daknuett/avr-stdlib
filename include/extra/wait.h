41d702498a2930901a7fc702d3288ee44f833989
Daniel Knuettel <daknuett@gmail.com>
2016-02-22 20:36:10 +0100

    moved wait.h

--- include/extra/wait.h
+++ include/extra/wait.h
@@ -23,6 +23,8 @@
  Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
 */
+
+/* vim: set syntax=c   */
 #ifndef __WAIT_H_
 #define __WAIT_H_
 
