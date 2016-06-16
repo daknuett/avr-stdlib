00d585b0bc9f78347c043d9acf87da7f52ab3e20
Daniel Knuettel <daknuett@gmail.com>
2016-06-16 20:10:51 +0200

    bl3_test working

--- bl3_test.c
+++ bl3_test.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	BL3MotorFD my_motor = bl3_motor_begin(11, 12,13, 14,15,  16,17);
+	bl3_motor_current_select(my_motor, 100);
 
 	while(1)
 	{
 		bl3_motor_step(my_motor);
-		_delay_ms(90);
+		_delay_ms(40);
 	}
 }
